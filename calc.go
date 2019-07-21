93a844c4e467b4237ecb15f83a05bf8f44da8126
Andrei Yangabishev <waffleboot@gmail.com>
2019-07-21 23:08:14 +0300

    config.go

--- calc.go
+++ calc.go
@@ -1,51 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"strconv"
-)
-
-type config struct {
-	work      int
-	small     int
-	large     int
-	worklimit int
-	timelimit int
-	mode      bool
-	verbose   bool
-}
-
-func parsehhmm(s string) int {
-	var hr, mi int
-	fmt.Sscanf(s, "%d:%d", &hr, &mi)
-	return hr*60 + mi
-}
-
-func newConfig() *config {
-
-	c := &config{}
-
-	flag.BoolVar(&c.mode, "w", false, "mode")
-	flag.BoolVar(&c.verbose, "v", false, "verbose")
-	flag.Parse()
-
-	read := func(arg int) int {
-		v, err := strconv.ParseInt(flag.Arg(arg), 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		return int(v)
-	}
-	c.work = read(0)
-	c.small = read(1)
-	c.large = read(2)
-	c.worklimit = read(3)
-	c.timelimit = parsehhmm(flag.Arg(4))
-
-	return c
-}
-
 type typ int
 
 const (
